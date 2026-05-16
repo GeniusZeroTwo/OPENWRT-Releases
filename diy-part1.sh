01dbb353842b1adf9e17a40210f15a7f09d050af
GeniusZeroTwo <geniusmmc@icloud.com>
2026-05-17 02:08:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# 描述: OpenWrt DIY script part 1 (Before Update feeds)
-
-# 1. 将 OpenClash 官方库作为源直接挂载
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
-
-# 2. 将 Aurora 主题官方库作为源直接挂载
-echo 'src-git aurora https://github.com/eamonxg/luci-theme-aurora.git' >> feeds.conf.default
